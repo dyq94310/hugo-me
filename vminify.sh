a3af41fa630898d98d2699509cc155cc189b84a9
dyq94310 <dyq94310@foxmail.com>
2025-12-08 11:12:38 +0800

    bug fix

--- vminify.sh
+++ vminify.sh
@@ -3,4 +3,4 @@ docker compose --profile build run --rm hugo hugo --minify
 
 #
 echo "cp public site to ngixn"
-cp -r public/ /opt/k3s-data/nginx-compose/public/
+cp -r public/ /opt/k3s-data/nginx-compose/
