2bc1d7f80ef850ce11c50a956bfa46372c67eb45
dyq94310 <dyq94310@foxmail.com>
2025-12-08 10:58:24 +0800

    to k3s

--- vminify.sh
+++ vminify.sh
@@ -3,4 +3,4 @@ docker compose --profile build run --rm hugo hugo --minify
 
 #
 echo "cp public site to ngixn"
-cp -r public/ ~/docker/nginx-compose/
+cp -r public/ ~//opt/k3s-data/nginx-compose/public/
